8dcd8e0f29e21cff9cae7cb31d9adc097e834873
Raunak Mondal <spmondal08@gmail.com>
2023-11-02 08:05:43 -0700

    Update Person.java

--- src/main/java/com/nighthawk/hacks/classDataStruct/Person.java
+++ src/main/java/com/nighthawk/hacks/classDataStruct/Person.java
@@ -6,23 +6,19 @@ import java.time.Period;
 import java.time.ZoneId;
 import java.util.Date;
 
-
 /*
 Adapted from Person POJO, Plain Old Java Object.
- */
-public class Person extends Generics{
+*/
+public class Person extends Generics {
     // Class data
     private static String classType = "Person";
     public static KeyTypes key = KeyType.title;  // static initializer
-	public static void setOrder(KeyTypes key) {Person.key = key;}
-	public enum KeyType implements KeyTypes {title, uid, name, dob, age}
 
     // Instance data
     private String uid;  // user / person id
     private String password;
     private String name;
     private Date dob;
-    
 
     // Constructor with zero arguments
     public Person() {
@@ -31,39 +27,46 @@ public class Person extends Generics{
 
     // Constructor used when building object from an API
     public Person(String uid, String password, String name, Date dob) {
-        this();  // runs zero argument constructor
+        this();  // runs zero-argument constructor
         this.uid = uid;
         this.password = password;
         this.name = name;
         this.dob = dob;
     }
 
+    // Add a method to check authentication
+    public boolean authenticate(String username, String password) {
+        return this.uid.equals(username) && this.password.equals(password);
+    }
+
     /* 'Generics' requires getKey to help enforce KeyTypes usage */
-	@Override
-	protected KeyTypes getKey() { return Person.key; }
+    @Override
+    protected KeyTypes getKey() {
+        return Person.key;
+    }
 
     public String getUserID() {
         return uid;
     }
 
     /* 'Generics' requires toString override
-	 * toString provides data based off of Static Key setting
-	 */
-	@Override
-	public String toString() {		
-		String output="";
-		if (KeyType.uid.equals(this.getKey())) {
-			output += this.uid;
-		} else if (KeyType.name.equals(this.getKey())) {
-			output += this.name;
-		} else if (KeyType.age.equals(this.getKey())) {
-			output += "0000" + this.getAge();  // pads integer 1,100,11,2 to 0001,0100,0011,0002
-			output = output.substring(output.length() - 4);
-		} else {
-			output = super.getType() + ": " + this.uid + ", " + this.name + ", " + this.getAge();
-		}
-		return output;
-	}
+     * toString provides data based off of Static Key setting
+     */
+    @Override
+    public String toString() {
+        String output = "";
+        if (KeyType.uid.equals(this.getKey())) {
+            output += this.uid;
+        } else if (KeyType.name.equals(this.getKey())) {
+            output += this.name;
+        } else if (KeyType.age.equals(this.getKey())) {
+            output += "0000" + this.getAge();  // pads integer 1,100,11,2 to 0001,0100,0011,0002
+            output = output.substring(output.length() - 4);
+        } else {
+            output = super.getType() + ": " + this.uid + ", " + this.name + ", " + this.getAge();
+        }
+        return output;
+    }
 
     public void setUid(String uid) {
         this.uid = uid;
@@ -97,13 +100,13 @@ public class Person extends Generics{
     public int getAge() {
         if (this.dob != null) {
             LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            return Period.between(birthDay, LocalDate.now()).getYears(); }
+            return Period.between(birthDay, LocalDate.now()).getYears();
+        }
         return -1;
     }
 
-    // Initialize static test data 
+    // Initialize static test data
     public static Person[] init() {
-
         // basics of class construction
         Person p1 = new Person();
         p1.setName("Thomas Edison");
@@ -146,7 +149,7 @@ public class Person extends Generics{
                 "Madam Currie", 
                 new SimpleDateFormat("MM-dd-yyyy").parse("01-01-2023")
             );
-    
+
             p5 = new Person(
                 "jm1021@gmail.com", 
                 "123Qwerty!",
@@ -157,8 +160,8 @@ public class Person extends Generics{
         }
 
         // Array definition and data initialization
-        Person persons[] = {p1, p2, p3, p4, p5};
-        return(persons);
+        Person persons[] = { p1, p2, p3, p4, p5 };
+        return persons;
     }
 
     public static void main(String[] args) {
@@ -166,10 +169,23 @@ public class Person extends Generics{
         Person persons[] = init();
         Person.setOrder(Person.KeyType.title);
 
-        // iterate using "enhanced for loop"
-        for( Person person : persons ) {
-            System.out.println(person);  // print object
+        // Sample login credentials
+        String username = "toby@gmail.com";
+        String password = "123Toby!";
+
+        // Attempt to log in
+        boolean loggedIn = false;
+        for (Person person : persons) {
+            if (person.authenticate(username, password)) {
+                loggedIn = true;
+                break;
+            }
+        }
+
+        if (loggedIn) {
+            System.out.println("Login successful.");
+        } else {
+            System.out.println("Invalid username or password. Please try again.");
         }
     }
-
 }
